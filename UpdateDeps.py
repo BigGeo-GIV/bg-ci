7299e43deba257ad625a257d8f11f1355cae38f3
Benjamin Ulmer <benjaminulmer55@gmail.com>
2023-04-28 10:24:33 -0600

    Use private JFrog for dep checking

--- UpdateDeps.py
+++ UpdateDeps.py
@@ -1,5 +1,6 @@
 import subprocess
 import json
+import sys
 
 from semantic_version import Version
 from semantic_version import NpmSpec
@@ -13,14 +14,16 @@ if __name__ == "__main__":
     for n, v in deps.items():
         curRange = v["range"]
         curVer = v["current"]
-        result = subprocess.run(["conan", "search", n, "-r=conancenter", "--raw"], capture_output=True, text=True)
+        result = subprocess.run(["conan", "search", n, f"-r={sys.argv[1]}", "--raw"], capture_output=True, text=True)
 
         packages = result.stdout.strip().split("\n")
-
         for package in packages:
             version = package.split("/")[1]
-            if Version(version) > Version(curVer) and Version(version) in NpmSpec(curRange):
-                newDeps[n] = version
+            try:
+                if Version(version) > Version(curVer) and Version(version) in NpmSpec(curRange):
+                    newDeps[n] = version
+            except:
+                pass
 
     if len(newDeps) == 0:
         exit(0)
