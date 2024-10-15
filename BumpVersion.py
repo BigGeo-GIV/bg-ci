63b601679e6ed68aaae5c50d9a9725e180a5ab8e
Benjamin Ulmer <benjaminulmer55@gmail.com>
2024-10-15 11:46:44 -0600

    work on new version bumping

--- BumpVersion.py
+++ BumpVersion.py
@@ -5,75 +5,105 @@ import os
 def strtobool(value: str) -> bool:
     return value.lower() in ("y", "yes", "on", "1", "true", "t")
 
-def commitJsonToIdx(commitJson, allowMajor):
-    minFound = 10
-    for commit in commitJson["commits"]:
-        found = commitTxtToIdx(commit["messageHeadline"], allowMajor, False)
-        minFound = min(minFound, found)
-    return minFound
-
-def commitTxtToIdx(commitText, allowMajor, alwaysBump):
+def commitTxtToIdxKey(commitText, allowMajor, alwaysBump):
     msg = commitText
-
     cType = msg.split(":")[0].lower()
-    if cType[-1] == "!": # not really working right now: or "BREAKING CHANGE" in msg:
-        if allowMajor:
-            return  0
-        return 1
-    elif "feat" in cType:
-        return 1
-    elif "fix" in cType:
-        return 2
-    elif "infra" in cType:
-        return 10
-    elif "docs" in cType:
-        return 10;
-    else:
-        return 1 if alwaysBump else 10 # bump minor if commit didn't match a pattern just to be safe
 
-def bumpSemVerList(semVer, idx):
-    semVer[idx] += 1
-    i = idx + 1
-    while i < len(semVer):
-        semVer[i] = 0
-        i += 1
+    idx = None
+
+    if cType[-1] == "!":
+        idx = 0 if allowMajor else 1
+    elif "feat" in cType:
+        idx = 1
+    elif "fix" in cType:
+        idx = 2
+    elif "infra" in cType:
+        idx = None
+    elif "docs" in cType:
+        idx = None;
+    else:
+        idx = 1 if alwaysBump else None # bump minor if commit didn't match a pattern just to be safe
+
+    scope = "default"
+    if "(" in cType and ")" in cType:
+        scope = cType.split("(")[1][0:-1].lower()
+
+    return (scope, idx)
+
+def commitJsonToBumps(commitJson, allowMajor):
+    bumps = {}
+
+    for commit in commitJson["commits"]:
+        (scope, idx) = commitTxtToIdxKey(commit["messageHeadline"], allowMajor, False)
+        if idx is not None:
+            if scope in bumps:
+                bumps[scope] = min(bumps[scope], idx)
+            else:
+                bumps[scope] = idx
+
+    return bumps
 
 def bumpSemVerStr(semVerStr, idx):
-    semVer = list(map(int, semVerStr.split(".")))
+    def bumpSemVerList(semVer, idx):
+        semVer[idx] += 1
+        i = idx + 1
+        while i < len(semVer):
+            semVer[i] = 0
+            i += 1
+
+    def intStrip(inNum):
+        return int(inNum.split("-")[0])
+
+    semVer = list(map(intStrip, semVerStr.split(".")))
     bumpSemVerList(semVer, idx)
     return ".".join(list(map(str, semVer)))
 
-if __name__ == "__main__":
-    path = sys.argv[1]
-    commitFilePath = sys.argv[2]
-    baseName = sys.argv[3]
-    dev = strtobool(sys.argv[4])
+def performBumps(bumps, verJson, dev):
+    baseName = "dev" if dev else "v"
 
+    first = True
+    for (scope, idx) in bumps.items():
+        if scope != "default":
+            name = scope + "-" + baseName
+        else:
+            name = baseName
+        oldVer = verJson[name]
+        newVer = bumpSemVerStr(oldVer, idx)
+        verJson[name] = newVer
+
+        if dev:
+            msg = name + "-" + str(newVer)
+        else:
+            msg = name + str(newVer)
+        if (not first):
+            print("; ", end="")
+
+        print(msg, end="")
+        first = False
+    print("")
+
+def main(jsonPath, commitFilePath, dev):
     ext = os.path.splitext(commitFilePath)[1]
 
+    bumps = {}
     if ext == ".json":
-        idx = commitJsonToIdx(json.load(open(commitFilePath)), not dev)
+        bumps = commitJsonToBumps(json.load(open(commitFilePath)), not dev)
     elif ext == ".txt":
-        idx = commitTxtToIdx(''.join(open(commitFilePath).readlines()), not dev, True)
+        (scope, idx) = commitTxtToIdxKey(''.join(open(commitFilePath).readlines()), not dev, True)
+        if idx is not None:
+            bumps[scope] = idx
     else:
         print("unrecognized commit file extension")
         exit(1)
 
-    if idx == 10:
-        print("nobump")
-        exit(0)
-
-    if dev:
-        name = baseName + "-dev"
-    else:
-        name = baseName
-
-    verJson = json.load(open(path))
-    oldVer = verJson[name]
-    newVer = bumpSemVerStr(oldVer, idx)
-    verJson[name] = newVer
-
-    out = open(path, "w")
-    json.dump(verJson, out)
+    verJson = json.load(open(jsonPath))
+    performBumps(bumps, verJson, dev)
+    out = open(jsonPath, "w")
+    json.dump(verJson, out, indent=4)
     out.write("\n")
-    print("bump")
+
+if __name__ == "__main__":
+    jsonPath = sys.argv[1]
+    commitFilePath = sys.argv[2]
+    dev = strtobool(sys.argv[3])
+    main(jsonPath, commitFilePath, dev)
