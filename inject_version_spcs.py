90173a158972ee72ca42acb9aa89829135d12d2c
Dawood Ali <55451465+dawood-ali@users.noreply.github.com>
2024-11-13 15:17:55 -0700

    Update inject_version_spcs.py

--- inject_version_spcs.py
+++ inject_version_spcs.py
@@ -45,19 +45,9 @@ with open(file_path, "r") as file:
         content = content.replace("IS_SNP: false", "IS_SNP: true")
         content = content.replace(
             "<image_src_w_prefix>", f"/spdemo/data_schema/alcyone_repository/")
-        content = content.replace("<openTelemetry_logging>",
-                                  '''- name: jaeger\r\n      image: /spdemo/data_schema/alcyone_repository/jaegertracing-all-in-one:1.58\r\n      env:\r\n        COLLECTOR_ZIPKIN_HOST_PORT: 9411\r\n        QUERY_BASE_PATH: /jaeger''')
+        content = content.replace("<openTelemetry_logging>","")
         content = content.replace(
             "OTEL_TRACING: <enable_tracing>", "OTEL_TRACING: true")
-        
-    elif branch == 'refs/heads/SP2':
-        content = content.replace("<env-tag>", "sp2")
-        content = content.replace("IS_SNP: false", "IS_SNP: true")
-        content = content.replace(
-            "<image_src_w_prefix>", f"/spdemo/data_schema/alcyone_repository/")
-        content = content.replace("<openTelemetry_logging>", "")
-        content = content.replace(
-            "OTEL_TRACING: <enable_tracing>", "OTEL_TRACING: false")
 
 
     else:
